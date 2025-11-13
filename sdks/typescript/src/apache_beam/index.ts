f079af87de88d5c17e14b77e7cd47df4d635bc4a
Martin Duhem <mduhem@gradle.com>
2025-11-13 11:11:47 +0100

    wip

--- sdks/typescript/src/apache_beam/index.ts
+++ sdks/typescript/src/apache_beam/index.ts
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// hello world
 
 export * from "./pvalue";
 export * from "./transforms";
